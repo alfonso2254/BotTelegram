e1d9532455519cce17118e81455d2e3d5bf33894
Luis Alfonso Alarcon <lalarcon@corebill.co>
2022-11-10 23:35:25 -0400

    First bot

---
+++ index.js
@@ -0,0 +1,40 @@
+
+// first bot telegram
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = '5673928106:AAElA19DDDMM5wPyCxwz_S5ZTTGZGu83uDs';
+const weather = require('weather-js');
+
+const bot = new TelegramBot(token, {polling: true});
+
+bot.onText(/^\/clima (.+)/, function(msg, match){
+    var chatId = msg.chat.id;
+    var ciudad = match[1];
+    console.log("🚀 ~ file: index.js ~ line 13 ~ bot.onText ~ ciudad", ciudad)
+
+    
+
+    var opciones = {
+        search: ciudad, // lugar es la ciudad que el usuario introduce
+        degreeType: 'C', // Celsius
+        lang: 'es-ES' // Lenguaje en el que devolverá los datos
+    }
+
+    weather.find(opciones, function(err, result){
+
+        if (err){ // Si ocurre algun error...
+            console.log(err); // ... nos lo muestra en pantalla
+
+        } else {
+            console.log(result[0]); // Visualizamos el primer resultado del array
+            
+            bot.sendMessage(chatId, "Lugar: " + result[0].location.name +
+            "\n\nTemperatura: " + result[0].current.temperature + "ºC\n" +
+            "Visibilidad: " + result[0].current.skytext + "\n" +
+            "Humedad: " + result[0].current.humidity + "%\n" +
+            "Dirección del viento: " + result[0].current.winddisplay + "\n"
+            ,{parse_mode: 'Markdown'});
+
+        }
+    })
+});
